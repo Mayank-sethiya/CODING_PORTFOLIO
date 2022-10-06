83aeb90a8c01e1d9dbc3c2926d41bfb656b5be18
Mayank-sethiya <114764579+Mayank-sethiya@users.noreply.github.com>
2022-10-06 08:28:59 +0530

    Add files via upload

---
+++ Q5.cpp
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()
+{
+	int l,b,area;
+	printf("PLEASE ENTER THE LENGTH ");
+	scanf("%d",&l);
+	printf("PLEASE ENTER THE BREADTH ");
+	scanf("%d",&b);
+	area=l*b;
+	printf("THE AREA OF RECTANGLE WITH LENGTH= %d AND BREADTH= %d IS %d",l,b,area);
+	return 0;
+	}
